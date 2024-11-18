def724bd65b92d934614de8d871cea0608db471e
Thesub77 <d7fquiroz@gmail.com>
2024-11-18 20:38:21 +0000

    feat: Fix tabular connection authentication error
    Authentication error when trying to connect to tabular model has been fixed by setting up a local connection to the model and updaloading the API to a IIS and access to its endpoints Through Virtual machine' DNS

--- Utils/MyHelperClass.cs
+++ Utils/MyHelperClass.cs
@@ -17,7 +17,7 @@ namespace ApiCubeTB.Utils
 
 
         // Cadena de conexion del modelo tabular
-        string connectionStringTabular = "Provider=MSOLAP;Data Source=asazure://canadacentral.asazure.windows.net/ssastechbridge;Initial Catalog=TechBridgeTabular;User ID=isiazuest15@estu.unan.edu.ni;Password=SoyrealThesubjectD7F;Persist Security Info=True;Impersonation Level=Impersonate";
+        string connectionStringTabular = "Provider=MSOLAP;Data Source=localhost\\SSASTabular;Catalog=TechBridgeTabular;";
         // Metodo que crea una conexion al modelo tabular
         public AdomdConnection GetConnectionTabular()
         {
